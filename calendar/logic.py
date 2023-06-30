bdb767d5ad37e279e9f34798ee4e2432bd2ba4e9
LobsterX <lobster2@mail.ru>
2023-06-30 13:27:44 +0300

    Initial commit

---
+++ calendar/logic.py
@@ -0,0 +1,61 @@
+from typing import List
+
+import model
+import db
+
+
+TITLE_LIMIT = 60
+TEXT_LIMIT = 120
+
+
+class LogicException(Exception):
+    pass
+
+
+class EventLogic:
+    def __init__(self):
+        self._event_db = db.EventDB()
+
+
+    @staticmethod
+    def _validate_note(event: model.Event):
+        if event is None:
+            raise LogicException("note is None")
+        if event.title is None or len(event.title) > TITLE_LIMIT:
+            raise LogicException(f"title length > MAX: {TITLE_LIMIT}")
+        if event.text is None or len(event.text) > TEXT_LIMIT:
+            raise LogicException(f"text length > MAX: {TEXT_LIMIT}")
+        if event.date in db.EventDB.list():
+            raise LogicException(f"You can add only one event to the date")
+
+    def create(self, event: model.Event) -> str:
+        self._validate_note(event)
+        try:
+            return self._event_db.create(event)
+        except Exception as ex:
+            raise LogicException(f"failed CREATE operation with: {ex}")
+
+    def list(self) -> List[model.Event]:
+        try:
+            return self._event_db.list()
+        except Exception as ex:
+            raise LogicException(f"failed LIST operation with: {ex}")
+
+    def read(self, _id: str) -> model.Event:
+        try:
+            return self._event_db.read(_id)
+        except Exception as ex:
+            raise LogicException(f"failed READ operation with: {ex}")
+
+    def update(self, _id: str, event: model.Event):
+        self._validate_note(event)
+        try:
+            return self._event_db.update(_id, event)
+        except Exception as ex:
+            raise LogicException(f"failed UPDATE operation with: {ex}")
+
+    def delete(self, _id: str):
+        try:
+            return self._event_db.delete(_id)
+        except Exception as ex:
+            raise LogicException(f"failed DELETE operation with: {ex}")
